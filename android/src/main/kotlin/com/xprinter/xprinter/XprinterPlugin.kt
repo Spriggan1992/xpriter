7d3329954de06eb7a32c703b25f557f92bcf7e5c
Roman Kozlov <roman.kozlov@buzzwoo.de>
2023-12-21 19:09:35 +0700

    change sticker width

--- android/src/main/kotlin/com/xprinter/xprinter/XprinterPlugin.kt
+++ android/src/main/kotlin/com/xprinter/xprinter/XprinterPlugin.kt
@@ -14,7 +14,7 @@ import net.posprinter.IDeviceConnection
 import net.posprinter.POSConnect
 import net.posprinter.TSCConst
 import net.posprinter.TSCPrinter
-
+import net.posprinter.utils.BitmapProcess
 
 
 /** XprinterPlugin */
@@ -56,10 +56,12 @@ class XprinterPlugin: FlutterPlugin, MethodCallHandler {
 
   private fun printBitmap(bitmapBytes: ByteArray, amount: Int) {
     val bitmap = BitmapFactory.decodeByteArray(bitmapBytes, 0, bitmapBytes.size)
+    val bmpCompressed = BitmapProcess.compressBmpByYourWidth(bitmap, 1000);
+
     tscPrinter!!.sizeMm (1000.0, 1600.0)
       .gapMm(0.0, 0.0)
       .cls()
-      .bitmap(0, 0, TSCConst.BMP_MODE_OVERWRITE, 200, bitmap)
+      .bitmap(0, 0, TSCConst.BMP_MODE_OVERWRITE, 200, bmpCompressed)
       .print(amount)
     disconnect()
     }
