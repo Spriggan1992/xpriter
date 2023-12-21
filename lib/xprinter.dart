33af7ee57b6190083347ec85a14321fdfeca1fde
Roman Kozlov <roman.kozlov@buzzwoo.de>
2023-12-21 18:02:47 +0700

    update Xprinter class

--- lib/xprinter.dart
+++ lib/xprinter.dart
@@ -5,35 +5,23 @@ import 'xprinter_method_channel.dart';
 class Xprinter {
   final channel = MethodChannelXprinter();
 
-  bool _connected = false;
+  Future<bool> connect(String ip) async {
+    return await channel.connect(ip);
+  }
 
-  bool get isConnected => _connected;
-
-  // Future<bool> connect(String ip) async {
-  //   _connected = await channel.connect(ip);
-  //   return _connected;
-  // }
-
-  // Future<bool> sendCommand(String command) async {
-  //   _connected = await channel.sendCommand(command);
-  //   return _connected;
-  // }
-
-  // Future<bool> sendCommand(Uint8List bitmapBytes) async {
-  //   _connected = await channel.sendCommand(bitmapBytes);
-  //   return _connected;
-  // }
+  Future<bool> checkConnection() async {
+    return await channel.checkConnection();
+  }
 
   Future<bool> sendToPrint({
     required String ip,
     required Uint8List imageBytes,
     required int amount,
   }) async {
-    _connected = await channel.sendToPrint(
+    return await channel.sendToPrint(
       imageBytes: imageBytes,
       ip: ip,
       amount: amount,
     );
-    return _connected;
   }
 }
