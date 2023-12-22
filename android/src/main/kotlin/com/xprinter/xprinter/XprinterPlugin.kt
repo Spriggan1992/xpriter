038a7573365d94af24544323ba76e4a3fa272659
Roman Kozlov <roman.kozlov@buzzwoo.de>
2023-12-22 22:39:31 +0700

    add print_from_file method

--- android/src/main/kotlin/com/xprinter/xprinter/XprinterPlugin.kt
+++ android/src/main/kotlin/com/xprinter/xprinter/XprinterPlugin.kt
@@ -70,7 +70,7 @@ class XprinterPlugin: FlutterPlugin, MethodCallHandler {
     tscPrinter!!.sizeMm (70.0, 120.0)
       .gapMm(0.0, 0.0)
       .cls()
-      .bitmap(0, 0, TSCConst.BMP_MODE_XOR, 500, bitmap)
+      .bitmap(0, 0, TSCConst.BMP_MODE_XOR, 600, bitmap)
       .print(amount)
 //    disconnect()
   }
